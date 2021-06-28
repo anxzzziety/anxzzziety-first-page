f67335eb914da092f1e087c2010f61552311128b
Anxzzziety <anxzzziety@gmail.com>
2021-06-27 20:00:59 -0400

    js directory add

---
+++ js/gsm.js
@@ -0,0 +1,31 @@
+const photosG = [
+    {
+        name: 'Llamas',
+        location: 'Grotto Falls Trail Head',
+        photo: 'img/smoky/llamas.jpg'
+
+    },
+    {
+        name: 'Hidden Waterfall',
+        location: 'Middle Prong Trail',
+        photo: 'img/smoky/middle-prong.jpg'
+
+    },
+    {
+        name: 'Grotto Falls',
+        location: 'Grotto Falls Trail',
+        photo: 'img/smoky/grotto-falls.jpg'
+
+   
+
+    },
+    {
+        name: 'Abrams Falls',
+        location: 'Abrams Falls Trail, Cades Cove',
+        photo: 'img/smoky/abrams-falls.jpg'
+
+   
+
+    },
+
+]
