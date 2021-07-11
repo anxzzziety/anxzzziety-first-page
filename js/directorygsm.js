96c6dc95ee3ef23606ced87778cd0967779f4efd
Anxzzziety <anxzzziety@gmail.com>
2021-07-11 18:42:04 -0400

    gsm js updates 071021

--- js/directorygsm.js
+++ js/directorygsm.js
@@ -1,3 +1,6 @@
+/* to hide js code */
+
+
 const main = document.querySelector('main');
 let html = '';
     for (let i =0; i < photosG.length; i ++) {
@@ -6,7 +9,13 @@ let html = '';
             <h4>${photoG.name}</h4>
             <h5>${photoG.location}</h5>
             <img src="${photoG.photo}">
-        `;
+       `;
+           
     }
 
-main.insertAdjacentHTML('beforeend' , html);
+main.insertAdjacentHTML('beforeend' , html);  
+document.getElementById('gsmP').style.backgroundColor = 'lightgreen'
+
+
+
+
