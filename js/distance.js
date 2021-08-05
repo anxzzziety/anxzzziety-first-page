986a561f40eb6e46a1cff5b9e2839101c9f08bf7
Anxzzziety <anxzzziety@gmail.com>
2021-08-05 19:24:53 -0400

    add distance conversion to thai html

---
+++ js/distance.js
@@ -0,0 +1,18 @@
+function lengthConverterI(valNum) {
+    document.getElementById("outputcm").innerHTML=valNum/0.39370;
+  }
+function lengthConverterCM(valNum) {
+    document.getElementById("outputInches").innerHTML=valNum*0.39370;
+  }
+function lengthConverterF(valNum) {
+    document.getElementById("outputMeters").innerHTML=valNum/3.2808;
+  }
+function lengthConverterM(valNum) {
+    document.getElementById("outputFeet").innerHTML=valNum*3.2808;
+  }
+function lengthConverterMiles(valNum) {
+    document.getElementById("outputKilometers").innerHTML=valNum/0.62137;
+  }
+function lengthConverterKilometers(valNum) {
+    document.getElementById("outputMiles").innerHTML=valNum*0.62137;
+  }
